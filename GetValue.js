82d257d42e52caa94015518eae6090c36e74e762
red_L <git@gitee.com:studentwheat/WarGame.git>
2024-02-15 21:24:00 +0800

    feat: 初始化

--- GetValue.js
+++ GetValue.js
@@ -1,8 +1,9 @@
-var UserName,Password;
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    UserName = document.getElementById('username').value;
-    Password = document.getElementById('password').value;
-});
-var myVariable = "Hello, World!";
-document.getElementById("myParagraph").innerHTML = myVariable;
+function disp_alert() 
+{ 
+    var form = document.getElementById('myForm');
+    var UserName = form.elements['username'].value,Password = form.elements['password'].value;
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+    });
+    alert("这位名为"+UserName+"的用户，您的密码是："+Password+"，猜猜接下来会发生什么呢？");
+} 
