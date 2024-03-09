dd18e6edb9902139d2b7058c598162917b48c051
red_L <git@gitee.com:studentwheat/WarGame.git>
2024-03-09 22:35:51 +0800

    add

--- GetValue.js
+++ GetValue.js
@@ -1,11 +1,10 @@
 function DispAlert() 
 { 
     window.location.href="https://lzcred.github.io/learning"
-    sleep(2000);
     var form = document.getElementById('myForm');
     var UserName = form.elements['username'].value,Password = form.elements['password'].value;
     form.addEventListener('submit', function(event) {
         event.preventDefault();
     });
-    alert("这位名为"+UserName+"的用户，您的密码是："+Password+"，相信你也知道后果了。");
+    alert("这位名为"+UserName+"的用户，您的密码是："+Password+"，请妥善保管您的密码。");
 } 
